99108a7f8bdc2112a345d842227530dbcd8fcdf7
18wh1a1235 <69300384+18wh1a1235@users.noreply.github.com>
2021-02-06 20:07:36 +0530

    Create Character_stuffing.c

---
+++ CN&WT lab/CN experiments/Experiment 1/Character_stuffing.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i=0,count=0,j=0,n;
+    char bits[20];
+    char stuffing[20];
+    printf("enter Data: ");
+    scanf("%s",bits);
+    printf("AT SENDER \n");
+    printf("before Character Stuffing:%s\n",bits);
+    printf("AT RECEIVER\n");
+
+    printf(" after Character stuffing:");
+    printf("DLESTX");
+    for(i=0; i<strlen(bits);i++)
+    {
+
+        if(bits[i] == 'D' && bits[i+1]== 'L' && bits[i+2] == 'E'){
+            printf("DLE");
+            j=j+3;
+
+        }
+        printf("%c",bits[i]);
+        stuffing[j]=bits[i];
+        j++;
+
+    }
+    printf("DLEETX\n");
+    printf("After Destuffing:");
+    for(i = 0; i < j; i++)
+    {
+        if(stuffing[i] == 'D' && stuffing[i] == 'L' && stuffing[i] == 'E'){
+            i=i+3;
+
+        }
+        printf("%c",stuffing[i]);
+
+    }
+    printf("\n");
+    return 0;
+
+}
