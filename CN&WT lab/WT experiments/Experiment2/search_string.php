b861ed7b78cb976062d31e194188b2e568841ffc
18wh1a1235 <69300384+18wh1a1235@users.noreply.github.com>
2021-06-10 15:53:43 +0530

    Create search_string.php

---
+++ CN&WT lab/WT experiments/Experiment2/search_string.php
@@ -0,0 +1,13 @@
+<?php
+
+$str1 = 'Welcome to WT Lab';
+if (strpos($str1,'Lab') !== false) 
+ {
+    echo 'The specific word is present.';
+ }
+else
+ {
+    echo 'The specific word is not present.';
+ }
+
+?>
